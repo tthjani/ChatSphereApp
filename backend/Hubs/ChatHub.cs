c0d22a1558b627a5dced77f4ec283ec3f34de0a5
tthjani <tothje98@gmail.com>
2024-08-16 11:16:22 +0200

    Add ChatHub class and UserConnection

---
+++ backend/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks.Dataflow;
+using ChatSphere.DataService;
+using ChatSphere.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSphere.Hubs;
+
+public class ChatHub : Hub
+{
+   private readonly SharedDb _shared;
+   
+   public ChatHub(SharedDb shared) => _shared = shared;
+   
+   public async Task JoinChat(UserConnection connection)
+   {
+      await Clients.All
+         .SendAsync("RecieveMessage", $"{connection.Username} has joined.");
+   }
+
+   public async Task JoinSpecificChatRoom(UserConnection connection)
+   {
+      await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+      
+      _shared.Connections[Context.ConnectionId] = connection;
+      
+      await Clients.Group(connection.ChatRoom)
+         .SendAsync("JoinSpecificChatRoom", $"{connection.Username} has joined to this room.");
+   }
+
+   public async Task SendMessage(string message)
+   {
+      if (_shared.Connections.TryGetValue(Context.ConnectionId, out UserConnection connection))
+      {
+         await Clients.Group(connection.ChatRoom)
+            .SendAsync("RecieveSpecificMessage", connection.Username, message);
+      }
+   }
+}
---
+++ backend/Models/UserConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatSphere.Models;
+
+public class UserConnection
+{
+    public string Username { get; set; } = string.Empty;
+    public string ChatRoom { get; set; } = string.Empty;
+}
